86af96b669c5e775f08ca8389cdc7b25672d6245
rb <rkrb@tyfonmail.se>
2024-07-30 09:32:06 +0200

    test image added and implemented

--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -12,7 +12,7 @@ export function Playlist({playlistBoxContainer}: IPlaylistProps): ReactElement {
         <div className="playlist-box">
             {playlistBoxContainer.data.map((playlistBox) => (
                 <p className="column" key={playlistBox.id}>
-                    <span>{playlistBox.image}</span>
+                    <span><img src={playlistBox.image} alt={playlistBox.band}/></span>
                     <span>{playlistBox.band}</span>
                     <span>{playlistBox.song}</span>
                     <PlayButton/>
--- src/data.ts
+++ src/data.ts
@@ -8,14 +8,14 @@ export const SongList = {
     songBoxes: {
         data:
             [
-                {id: 1, image: "img", band: "band", song: "sång", duration: 59},
-                {id: 2, image: "img", band: "band nummer två", song: "sången", duration: 59},
-                {id: 3, image: "img", band: "band", song: "sång", duration: 59},
-                {id: 4, image: "img", band: "bandet", song: "sången", duration: 59},
-                {id: 5, image: "img", band: "band", song: "sång", duration: 59},
-                {id: 6, image: "img", band: "bandet", song: "sången", duration: 59},
-                {id: 7, image: "img", band: "band", song: "sång", duration: 59},
-                {id: 8, image: "img", band: "bandet", song: "sången", duration: 59},
+                {id: 1, image: "src/assets/images/testImage.png", band: "band", song: "sång", duration: 59},
+                {id: 2, image: "src/assets/images/testImage.png", band: "band nummer två", song: "sången", duration: 59},
+                {id: 3, image: "src/assets/images/testImage.png", band: "band", song: "sång", duration: 59},
+                {id: 4, image: "src/assets/images/testImage.png", band: "bandet", song: "sången", duration: 59},
+                {id: 5, image: "src/assets/images/testImage.png", band: "band", song: "sång", duration: 59},
+                {id: 6, image: "src/assets/images/testImage.png", band: "bandet", song: "sången", duration: 59},
+                {id: 7, image: "src/assets/images/testImage.png", band: "band", song: "sång", duration: 59},
+                {id: 8, image: "src/assets/images/testImage.png", band: "bandet", song: "sången", duration: 59},
             ]
     }
 }
