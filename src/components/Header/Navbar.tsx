760c892a4b4335807d6d9ee069bf6d1beb5e6681
rb <rkrb@tyfonmail.se>
2024-07-30 09:22:36 +0200

    css navbar

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -2,8 +2,7 @@ import {ReactElement} from "react";
 import {BackArrow} from "./BackArrow.tsx";
 import {navbar} from "../../data.ts";
 import {HamburgerMenu} from "./HamburgerMenu.tsx";
-
-
+import "./Header.css"
 
 export function Navbar(): ReactElement
 { return (
