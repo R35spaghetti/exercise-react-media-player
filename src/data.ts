7af99d2aebc0022bbb5467430eec98087ebd9799
rb <rkrb@tyfonmail.se>
2024-07-31 13:46:15 +0200

    replaced images, adjusted size

--- src/data.ts
+++ src/data.ts
@@ -9,13 +9,13 @@ export const SongList = {
         data:
             [
                 {id: 1, image: "src/assets/images/testImage.png", band: "The Nebula Navigators", song: "Ethereal Echoes", duration: 59},
-                {id: 2, image: "src/assets/images/testImage.png", band: "Quantum Quirk Quartet", song: "Quantum Quiver", duration: 59},
-                {id: 3, image: "src/assets/images/testImage.png", band: "Cosmic Cacophony Collective", song: "Nebulous Nightfall", duration: 59},
-                {id: 4, image: "src/assets/images/testImage.png", band: "Galactic Groove Guild", song: "Galactic Glide", duration: 59},
-                {id: 5, image: "src/assets/images/testImage.png", band: "Interstellar Impromptu Inc", song: "Interstellar Interlude", duration: 59},
-                {id: 6, image: "src/assets/images/testImage.png", band: "Astrological Anomaly Alliance", song: "Astrological Afterglow", duration: 59},
-                {id: 7, image: "src/assets/images/testImage.png", band: "Celestial Chorus Company", song: "Celestial Cadence", duration: 59},
-                {id: 8, image: "src/assets/images/testImage.png", band: "Stellar Symphony Society", song: "Stellar Serenade", duration: 59},
+                {id: 2, image: "src/assets/images/bild2.png", band: "Quantum Quirk Quartet", song: "Quantum Quiver", duration: 59},
+                {id: 3, image: "src/assets/images/bild3.png", band: "Cosmic Cacophony Collective", song: "Nebulous Nightfall", duration: 59},
+                {id: 4, image: "src/assets/images/bild4.png", band: "Galactic Groove Guild", song: "Galactic Glide", duration: 59},
+                {id: 5, image: "src/assets/images/bild5.png", band: "Interstellar Impromptu Inc", song: "Interstellar Interlude", duration: 59},
+                {id: 6, image: "src/assets/images/bild6.png", band: "Astrological Anomaly Alliance", song: "Astrological Afterglow", duration: 59},
+                {id: 7, image: "src/assets/images/bild7.png", band: "Celestial Chorus Company", song: "Celestial Cadence", duration: 59},
+                {id: 8, image: "src/assets/images/bild8.png", band: "Stellar Symphony Society", song: "Stellar Serenade", duration: 59},
             ]
     }
 }
