df45ab88db9f08f3749321ccb59c289b73e53828
rb <rkrb@tyfonmail.se>
2024-07-31 14:20:25 +0200

    used up the white space in the play button

--- src/components/Playlist/PlayButton.tsx
+++ src/components/Playlist/PlayButton.tsx
@@ -2,6 +2,6 @@ import {ReactElement} from "react";
 
 export function PlayButton({onClick}: { onClick: () => void }): ReactElement {
     return (
-        <span className="material-symbols-outlined" onClick={onClick}>play_circle</span>
+        <span className="material-symbols-outlined" onClick={onClick}>play_circle<p>play</p></span>
     );
 }
